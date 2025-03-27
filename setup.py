59dd009f5822fcc772ef9d46ebaa4d111087e8b8
smarakaranjan <smarakaranjan@dataterrain.com>
2025-03-27 12:43:04 +0530

    Fix Package issues

--- setup.py
+++ setup.py
@@ -1,23 +1,24 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="django-mqtt-service",
+    name="RealTimeMQ",
     version="0.1.0",
-    packages=find_packages(),
-    include_package_data=True,
+    packages=find_packages(),  # Auto-detects all packages
     install_requires=[
-        "django>=3.2",
-        "paho-mqtt>=1.6.1"
+        "paho-mqtt",
+        "Django>=3.2",
     ],
-    author="Your Name",
-    author_email="your_email@example.com",
-    description="A Django plugin for real-time MQTT-based messaging and notifications",
+    include_package_data=True,
+    description="A pluggable Django app for MQTT-based chat and notifications.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/smarakaranjan/RealTimeMQ.git",
+    url="https://github.com/smarakaranjan/RealTimeMQ",
+    author="Your Name",
+    author_email="your_email@example.com",
+    license="MIT",
     classifiers=[
-        "Framework :: Django",
         "Programming Language :: Python :: 3",
+        "Framework :: Django",
         "License :: OSI Approved :: MIT License",
     ],
 )
